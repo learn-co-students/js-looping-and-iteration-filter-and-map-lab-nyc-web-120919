9467ced6dc0c2ebc79e7d2e094b0962441f38b13
flatironschool <danliu277@gmail.com>
2020-01-27 13:41:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+    return drivers.filter((driver) => {
+        return driver.revenue > revenue;
+    });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+    return driversWithRevenueOver(drivers, revenue)
+        .map((driver) => {
+            return driver.name;
+        });
+}
+
+function exactMatch(drivers, matcher) {
+    return drivers.filter((driver) => {
+        let matches = false;
+
+        for (const key in matcher) {
+            matches = driver[key] === matcher[key];
+        }
+
+        return matches;
+    });
+}
+
+function exactMatchToList(drivers, matcher) {
+    return exactMatch(drivers, matcher)
+        .map((driver) => {
+            return driver.name;
+        });
+}
